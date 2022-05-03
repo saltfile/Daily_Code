acaa8204726294b307b6c6a91c2c5fe4de14a236
saltfile <3205617363@qq.com>
2022-05-03 16:23:20 +0800

    05-02

---
+++ demoADUS/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,65 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.userDao;
+import com.example.demo.entiry.user_Tab;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService implements userservicempl{
+    @Autowired
+    private userDao userDao;
+
+    /**
+     * TODO:登录业务
+     * @param id 用户名
+     * @param password 密码
+     * @return 找到得用户
+     */
+    @Override
+    public user_Tab Login(String id, String password) {
+        return userDao.Sel(id,password);
+    }
+
+    /**
+     * TODO:注册业务
+     * @param user 要注册的用户
+     * @return 是否成功
+     */
+    @Override
+    public boolean Register(user_Tab user) {
+        return userDao.Add(user) > 0;
+    }
+
+    /**
+     * TODO:展示所有用户
+     * @return
+     */
+    @Override
+    public ArrayList<user_Tab> Show_All() {
+
+        return userDao.ShowAll();
+    }
+
+    /**
+     * TODO:删除某个用户
+     * @param id 要删除的用户id
+     * @return 是否成功
+     */
+    @Override
+    public boolean Delete(String id) {
+        return userDao.Del(id) > 0;
+    }
+
+    /**
+     * TODO：个人用户修改信息
+     * @param user 被修改的用户
+     * @return 是否成功
+     */
+    @Override
+    public boolean Update(user_Tab user) {
+        return userDao.Update(user) > 0;
+    }
+}
---
+++ demoADUS/src/main/resources/static/js/request/user.js
@@ -0,0 +1,21 @@
+$(function () {
+  $('#Lon_Btn').on('click',function () {
+    $.ajax({
+        url: "/Lon",
+        method: "post",
+        data: {'id': $("#uid").val(),'password': $("#pwd").val()},
+        // xhrFields: {
+        //   withCredentials: true //允许跨域带Cookie
+        // },
+        success: function (result) {
+            if(result == "Y")
+                alert("普用用户登录成功");
+            id(result == "AY")
+                alert("管理员登陆成功");
+        },
+        error: function (result) {
+            alert(result.responseText)
+        }
+    })
+});
+})
