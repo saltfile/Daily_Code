c04a0b7925d64ac0e9a16146d03c222358697c0f
saltfile <3205617363@qq.com>
2022-05-12 20:58:21 +0800

    05-11

--- vpn/main.cpp
+++ vpn/main.cpp
@@ -1,45 +1,103 @@
+
 #include "MyAll.h"
+/**
+ * std::future它表示存储着一个未来会被初始化的变量。
+ * 这个变量可以通过std::future提供的成员函数std::future::get()来得到。
+ * 如果在这个变量被赋值之前就有别的线程试图通过std::future::get()获取这个变量，那么这个线程将会被阻塞到这个变量可以获取为止。
+ * std::promise对象都有一个与之关联的std::future对象。
+ * 当std::promise设置值的时候，这个值就会赋给std::future中的对象了
+ * @param x
+ * @param y
+ * @param promiseObj
+ */
+void func2(int x, int y,promise<char *> &promiseObj) {
+    char *res = "";
+    if(x == 1){
+        res = "aaa";
+    }
+    if (x!= 1){
+        res = "bbb";
+    }
+    promiseObj.set_value(res);
+}
 
 
-#define PORT 8686			//目标地址端口号
-#define ADDR "localhost" //目标地址IP
+//
+//int main()
+//{
+//char *str = "localhost";
+//thread t(cil_start,str,8686);
+//t.join();
 
+
+#define PORT 8181	//端口号
+//#define BACKLOG 5	//最大监听数
+//thread_local char *as = (char *)malloc(sizeof(1024));
 int main()
 {
-    int iSocketFD = 0; //socket句柄
-    unsigned int iRemoteAddr = 0;
-    struct sockaddr_in stRemoteAddr = {0}; //对端，即目标地址信息
-    socklen_t socklen = 0;
-    char buf[4096] = {0}; //存储接收到的数据
 
-    iSocketFD = socket(AF_INET, SOCK_STREAM, 0); //建立socket
-    if(0 > iSocketFD)
-    {
-        printf("创建socket失败！\n");
-        return 0;
-    }
+//    promise<char *> promiseObj2;
+//    future<char *> futureObj2 = promiseObj2.get_future();
+//        thread t(ser_start,8484);
+//        t.join();
+        char *sss = "GET /VpnLab_war_exploded/indeax.html HTTP/1.1\n"
+                    "Host: localhost:8484\n"
+                    "Connection: keep-alive\n"
+                    "sec-ch-ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\", \"Google Chrome\";v=\"101\"\n"
+                    "sec-ch-ua-mobile: ?0\n"
+                    "sec-ch-ua-platform: \"Linux\"\n"
+                    "Upgrade-Insecure-Requests: 1\n"
+                    "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36\n"
+                    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n"
+                    "Sec-Fetch-Site: none\n"
+                    "Sec-Fetch-Mode: navigate\n"
+                    "Sec-Fetch-User: ?1\n"
+                    "Sec-Fetch-Dest: document\n"
+                    "Accept-Encoding: gzip, deflate, br\n"
+                    "Accept-Language: zh-CN,zh;q=0.9\n"
+                    "Cookie: Idea-3571c926=a28cacb7-6273-4465-a8cb-550f550417e7";
+        char dest[1024];
+        memset(dest,0,1024);
+        char *port = "7878";
+        char *host = "localhost:";
+        host = str_merge(host,port);
+        strrpc(dest,sss,"localhost:8484",host);
+        cout<<dest<<endl;
+        cout<<strlen(dest)<<endl;
 
-    stRemoteAddr.sin_family = AF_INET;
-    stRemoteAddr.sin_port = htons(PORT);
-    inet_pton(AF_INET, ADDR, &iRemoteAddr);
-    stRemoteAddr.sin_addr.s_addr=iRemoteAddr;
 
-    //连接方法： 传入句柄，目标地址，和大小
-    if(0 > connect(iSocketFD, (const struct sockaddr *)&stRemoteAddr, sizeof(stRemoteAddr)))
-    {
-        printf("连接失败！\n");
-        //printf("connect failed:%d",errno);//失败时也可打印errno
-    }else{
+//char *sum = futureObj2.get();
+//cout<<"传回来的值"<<sum;
 
-        printf("连接成功！\n");
-        recv(iSocketFD, buf, sizeof(buf), 0);// 将接收数据打入buf，参数分别是句柄，储存处，最大长度，其他信息（设为0即可）。
-        printf("Received:%s\n", buf);
-        send(iSocketFD,buf,sizeof(buf),0);
+////计算（a+b）/(x+y)
+//    //用三个线程，一个线程计算a+b，另一个线程计算x+y
+//    int a, b, x, y;
+//    a = 1, b = 8, x = 2, y = 4;
+//
+//    char * sum1 = NULL;
+//    char *sum2 = NULL;
+//    //声明一个类
+//    std::promise<char *> promiseObj;
+//    //将future和promise关联
+//    std::future<char *> futureObj = promiseObj.get_future();
+//    //模板传参的时候使用ref，否则传参失败
+//    std::thread t1(func2, a, b, ref(promiseObj));
+//    t1.join();
+//    //获取值
+//    sum1 = str_copy(sum1,futureObj.get());
+//
+//    std::cout << "sum1=" << sum1 << std::endl;
+//
+//    //不能直接复用上面的future和promise
+//    promise<char *> promiseObj2;
+//    future<char *> futureObj2 = promiseObj2.get_future();
+//
+//    std::thread t2(func2, x, y, ref(promiseObj2));
+//    t2.join();
+//    sum2 = str_copy(sum2,futureObj2.get());
+//    std::cout << "sum2=" << sum2 << std::endl;
 
 
-    }
-
-    close(iSocketFD);//关闭socket
     return 0;
 }
 
--- vpn/server.cpp
+++ vpn/server.cpp
@@ -1,29 +1,101 @@
 //
 // Created by saltfish on 2022/5/11.
 //
+#include <mutex>
 #include "MyAll.h"
 /**
  * TODO:正常的c语言服务端开启
  * @param port:端口号
  * @return 只是代表结束，没什么意义
  * */
-int ser_start(int port){
-    int listenfd,connfd;
-    socklen_t client;
-    struct  sockaddr_in cliaddr,seraddr;
 
-    listenfd = socket(AF_INET,SOCK_STREAM,0);
-    bzero(&seraddr,sizeof(seraddr));
-    seraddr.sin_family = AF_INET;
-    seraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    seraddr.sin_port = htons(port);
 
-    bind(listenfd,(struct sockaddr *) &seraddr,sizeof(seraddr));
-    listen(listenfd,1024);
 
-    for(;;){
-        client = sizeof(cliaddr);
-        connfd = accept(listenfd,(struct  sockaddr *) &cliaddr,&client);
-//        read_runtable(connfd);
+std::mutex cout_mutex;
+int ser_start(int port) {
+    int iSocketFD = 0;  //socket句柄
+    int new_fd = 0;    //建立连接后的句柄
+    struct sockaddr_in stLocalAddr = {0}; //本地地址信息结构图，下面有具体的属性赋值
+    struct sockaddr_in stRemoteAddr = {0}; //对方地址信息
+    socklen_t socklen = 0;
+
+    iSocketFD = socket(AF_INET, SOCK_STREAM, 0); //建立socket
+    if (0 > iSocketFD) {
+        printf("创建socket失败！\n");
+        return 0;
+    }
+
+    stLocalAddr.sin_family = AF_INET;  /*该属性表示接收本机或其他机器传输*/
+    stLocalAddr.sin_port = htons(port); /*端口号*/
+    stLocalAddr.sin_addr.s_addr = htonl(INADDR_ANY); /*IP，括号内容表示本机IP*/
+
+    //绑定地址结构体和socket
+    if (0 > bind(iSocketFD, (struct sockaddr *) &stLocalAddr, sizeof(stLocalAddr))) {
+        printf("绑定失败！\n");
+        return 0;
+    }
+
+    //开启监听 ，第二个参数是最大监听数
+    if (0 > listen(iSocketFD, 128)) {
+        printf("监听失败！\n");
+        return 0;
+    }
+
+    printf("iSocketFD: %d\n", iSocketFD);
+    //在这里阻塞知道接收到消息，参数分别是socket句柄，接收到的地址信息以及大小
+    for(;;) {
+        new_fd = accept(iSocketFD, (struct sockaddr *) &stRemoteAddr, &socklen);
+        thread run(server_main,new_fd);
+        run.join();
+
+    }
+    close(new_fd);
+    close(iSocketFD);
+    return 0;
+}
+
+/**
+ * 这里读取出来的返回值需要返回
+ * @param promiseObj 返回数值表示对端是否断掉
+ * @param promiseObj2 返回的内容
+ */
+void rec_runtable(int new_fd, promise<int> &promiseObj, promise<char *> &promiseObj2) {
+    char buf[1024 * 4];
+    memset(buf,0,sizeof(buf));
+    int singal = recv(new_fd, buf, sizeof(buf), 0);
+    promiseObj.set_value(singal);
+    promiseObj2.set_value(buf);
+}
+
+void send_runtable(int new_fd,char *buf){
+send(new_fd, buf, sizeof(buf), 0);
+}
+
+int server_main(int new_fd){
+    int iRecvLen = 0;   //接收成功后的返回值
+    for (;;) {
+        if (0 > new_fd) {
+            printf("接收失败！\n");
+            return 0;
+        }
+        printf("new_fd: %d\n", new_fd);//分出一个线程处理接收
+
+
+        promise<int> singal;
+        promise<char *>readMes;
+        //分出一个线程处理读问题
+        thread t1(rec_runtable,new_fd,ref(singal),ref(readMes));
+        t1.join();
+        future<int> sin = singal.get_future();
+        future<char *> mes = readMes.get_future();
+        iRecvLen = sin.get();
+
+
+        if(0 >= iRecvLen)    //对端关闭连接 返回0
+        {
+            printf("接收失败或者对端关闭连接！\n");
+        }else{
+            printf("buf: %s\n", mes.get());
+        }
     }
 }
