1f115b9448c82dc46936eb6e9a34ae29b3d1886b
saltfile <3205617363@qq.com>
2022-07-07 21:08:32 +0800

    07-07

--- talkCheet/talk_ioFile/src/main/java/undo.java
+++ talkCheet/talk_ioFile/src/main/java/undo.java
@@ -8,8 +8,12 @@ import java.util.List;
 public class undo {
     public static void main(String[] args) {
         FileMethods.InitFilePath();
-
-
-
+        FileMethods.CacheInit();
+        FileMethods.DisPlayCache();
+//        FileMethods.AddFriendRequest("xxx","aaa");
+//        FileMethods.AssentRequest("xxx","aaa");
+        FileMethods.DeleteFriends("xxx","aaa");
+        FileMethods.DisPlayCache();
+        FileMethods.CacheEntry();
     }
 }
