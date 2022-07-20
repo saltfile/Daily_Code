53479ae0bb4daa3dfbe2ee76f0530e88281f6c8b
saltfile <3205617363@qq.com>
2022-07-20 23:03:55 +0800

    07-20

---
+++ SpringCloudTest/Cloud-Gataway/src/main/java/org/example/config/SpringsConfig.java
@@ -0,0 +1,23 @@
+package org.example.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringsConfig {
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(
+                        p -> p.path("/product/list").uri("http://localhost:8090")
+                )
+                .route(
+                        p -> p.path("/payment/**").uri("http://localhost:8001")
+                )
+                .build();
+    }
+
+
+}
