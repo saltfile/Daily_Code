0cac180784eb13ba3fc1c01058f0fc59ae6892e8
saltfile <3205617363@qq.com>
2022-07-23 23:13:04 +0800

    07-23

--- SpringCloudTest/Cloud-Gataway/src/main/java/org/example/config/SpringsConfig.java
+++ SpringCloudTest/Cloud-Gataway/src/main/java/org/example/config/SpringsConfig.java
@@ -7,17 +7,17 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringsConfig {
-    @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(
-                        p -> p.path("/product/list").uri("http://localhost:8090")
-                )
-                .route(
-                        p -> p.path("/payment/**").uri("http://localhost:8001")
-                )
-                .build();
-    }
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(
+//                        p -> p.path("/product/list").uri("http://localhost:8090")
+//                )
+//                .route(
+//                        p -> p.path("/payment/**").uri("http://localhost:8001")
+//                )
+//                .build();
+//    }
 
 
 }
