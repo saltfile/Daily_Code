784297e3d50139b63ae8e8c515e2ba9f67b40ba3
saltfile <3205617363@qq.com>
2022-10-21 21:00:44 +0800

    10-21

--- threadTest/expmlpe10.cpp
+++ threadTest/expmlpe10.cpp
@@ -22,23 +22,68 @@ int current_floor_number = 1;
 int accumulated_floor_number = 0;
 
 
-int event_idle(){}
-int event_goingup(){}
-int event_goingdown(){}
-int event_attop(){}
-int event_atbottom(){}
-int event_malfuntion(){}
-int event_unexpect(){}
-int event_end(){}
+int event_idle(){
+    cout<<"我是开机状态"<<endl;
+    sleep(5);
+
+    return up;
+}
+int event_goingup(){
+
+    cout<<"上行"<<endl;
+    sleep(3);
+
+    return bottom;
+}
+int event_goingdown(){
+
+    cout<<"下降"<<endl;
+    sleep(3);
+    return top;
+
+
+}
+int event_attop(){
+    cout<<"顶部"<<endl;
+    sleep(4);
+    return at_bottom;
+}
+int event_atbottom(){
+    cout<<"底部"<<endl;
+    sleep(6);
+    return mal_funtion;
+
+}
+int event_malfuntion(){
+    cout<<"结束"<<endl;
+    return _end;
+
+}
+int event_unexpect(){
+    cout<<"结束"<<endl;
+    return _end;
+}
+int event_end(){
+    cout<<"结束"<<endl;
+    return _end;
+}
 
 
 
 
+int lookup_transitions[][7] = {
+        // return codes:
+        //      up       down       halt          top         bottom         fail          quit
+        [idle]      = {going_up,    going_down,  idle,     un_expect, un_expect,  mal_funtion,  _end},
+        [going_up]   = {going_up,    un_expect, idle,     at_top,      at_bottom,    mal_funtion,  _end},
+        [going_down] = {un_expect, going_down,  idle,     at_top,      at_bottom,    mal_funtion,  _end},
+        [at_top]     = {un_expect, going_down,  at_top,     at_bottom, un_expect,  mal_funtion,  _end},
+        [at_bottom]  = {going_up,    going_down,  at_bottom, un_expect, un_expect,  mal_funtion,  _end},
+        [mal_funtion] = {_end, _end, _end, _end, _end, _end, _end},
+        [un_expect]  = {_end, _end, _end, _end, _end, _end, _end}
+};
 
-
-
-
-int expampe10_main(){
+void* runable(void *arg){
     enum stat_code now_stat = ENTRY_STAT;
     enum ret_code rc;
     int (*stat_func)(void);
@@ -49,10 +94,13 @@ int expampe10_main(){
         rc =(ret_code)stat_func();
         if (now_stat == END_STAT)
             break;
-
+        now_stat = (enum stat_code)lookup_transitions[now_stat][rc];
     }
 
     cout<<"有限状态机结束";
+    pthread_exit(0);
+
+}
 
 
 
@@ -60,7 +108,13 @@ int expampe10_main(){
 
 
 
+int expampe10_main(){
 
+    pthread_t pid;
+    pthread_create(&pid,NULL,runable,NULL);
+    void *ptr;
+    pthread_join(pid,&ptr);
+    cout<<"整体结束"<<endl;
     return 0;
 }
 
