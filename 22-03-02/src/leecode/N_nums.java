2d99d504226350cc99ccebc308965fc281d1bbc2
saltfish <3205617363@qq.com>
2022-11-22 22:04:57 +0800

    11-22

---
+++ 22-03-02/src/leecode/N_nums.java
@@ -0,0 +1,36 @@
+package leecode;
+
+public class N_nums {
+    public int nthMagicalNumber(int n, int a, int b) {
+        long l = Math.min(a, b);
+        long r = (long) n * Math.min(a, b);
+        int c = lcm(a, b);
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            long cnt = mid / a + mid / b - mid / c;
+            if (cnt >= n) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int) ((r + 1) % 1000000007);
+    }
+
+    public int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public int gcd(int a, int b) {
+        return b != 0 ? gcd(b, a % b) : a;
+    }
+
+
+    public static void main(String[] args) {
+       int a = new N_nums().nthMagicalNumber(1,2,3);
+        System.out.println(a);
+    }
+
+
+
+}
