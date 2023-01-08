afdcba79ed70fd134f23f2e74fbee577b955bfc0
saltfile <3205617363@qq.com>
2023-01-08 23:56:22 +0800

    01-08

--- 22-03-02/src/aohiothor/linklist_part.java
+++ 22-03-02/src/aohiothor/linklist_part.java
@@ -1,5 +1,8 @@
 package aohiothor;
 
+import javax.print.DocFlavor;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
@@ -239,8 +242,32 @@ class GetIntersectionNode{
 
 }
 
+/**
+ * 给定一个链表的头节点  head，返回链表开始入环的第一个节点。如果链表无环，则返回null。
+ *
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ *
+ * 不允许修改 链表。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/linked-list-cycle-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * 无所谓，Hashset会出手
+ */
+class DetectCycle{
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> list_set = new HashSet<>();
+        ListNode dey = head;
+        while (list_set.add(dey) != false){
+            if (dey == null) return null;
+            dey = dey.next;
+        }
+        return dey;
+    }
 
-
+}
 
 
 
@@ -262,6 +289,7 @@ public class linklist_part {
             p.next = new ListNode(i);
             p = p.next;
         }
+        p.next = n1.next.next;
 
 //        new ReverseList().reverseList2(n1);
 //        new  SwapPairs().swapPairs(n1);
