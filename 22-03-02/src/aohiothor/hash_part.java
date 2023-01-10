77482da0623e1e4b7e22968663b78940ad99949c
saltfile <3205617363@qq.com>
2023-01-10 20:57:27 +0800

    01-10

--- 22-03-02/src/aohiothor/hash_part.java
+++ 22-03-02/src/aohiothor/hash_part.java
@@ -1,6 +1,7 @@
 package aohiothor;
 
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -104,12 +105,64 @@ class IsHappy{
 
 }
 
+/**
+ * 1. 两数之和
+ *
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ *
+ * 你可以按任意顺序返回答案。
+ *  第一种方式我直接双循环莽过
+ *  第二种使用map过滤掉没必要的第二次循环
+ */
+class TwoSum{
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        if (nums.length == 0&&nums == null)return res;
 
 
+        for (int i = 0; i < nums.length; i++) {
+            int r = target - nums[i];
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j] == r){
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+    public int[] twoSum2(int[] nums, int target) {
+        int[] res = new int[2];
+        if (nums.length == 0&&nums == null)return res;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int ids = target-nums[i];
+            if (map.containsKey(ids)){
+                res[0] = map.get(ids);
+                res[1] = i;
+                break;
+            }
+            map.put(nums[i],i);
+
+        }
+        return res;
+    }
+}
 
 
+/**
+ * 454. 四数相加 II
+ *
+ * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+ */
 
+class FourSumCount{
 
+}
 
 
 
@@ -120,13 +173,14 @@ public class hash_part {
 //        System.out.println(new IsAnagram().isAnagram("nagaram","anagram"));
 
 
-        int[] n1 = {1,2,2,1};
-        int[] n2 = {2,2,3};
-        int[] res = new Intersection().intersection(n2,n1);
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i]+"     ");
-        }
-
+//        int[] n1 = {1,2,2,1};
+//        int[] n2 = {2,2,3};
+//        int[] res = new Intersection().intersection(n2,n1);
+//        for (int i = 0; i < res.length; i++) {
+//            System.out.print(res[i]+"     ");
+//        }
+        int[] n3 = {3,2,4};
+        System.out.println(new TwoSum().twoSum2(n3,6));
 
 
 
