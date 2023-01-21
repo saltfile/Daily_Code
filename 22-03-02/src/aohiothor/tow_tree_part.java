7517bf989e875fc78036c2177bbbdedb9a555c95
saltfile <3205617363@qq.com>
2023-01-21 21:49:28 +0800

    01-21

--- 22-03-02/src/aohiothor/tow_tree_part.java
+++ 22-03-02/src/aohiothor/tow_tree_part.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+
+
+
+//算法二叉树部分
 class TreeNode {
       int val;
       TreeNode left;
@@ -75,16 +79,69 @@ class Ergodic{
 }
 
 
+/**
+ * 102. 二叉树的层序遍历
+ *
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
+ */
+
+
+class LevelOrder{
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res =  new ArrayList<>();
+        int level = 0;
+        Order(root,res,level);
+        return res;
+
+    }
+    public void Order(TreeNode root,List<List<Integer>> res,int level){
+        if (root == null)return;
+        if (res.size() == level) res.add(new ArrayList<>());
+
+        res.get(level).add(root.val);
+
+        Order(root.left,res,level + 1);
+        Order(root.right,res,level + 1);
+
+    }
+
+
+}
+
+
+/**
+ * 226. 翻转二叉树
+ *
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 输出：[4,7,2,9,6,3,1]
+ */
+
+class InvertTree{
+    public TreeNode invertTree(TreeNode root) {
+        revcse(root);
+        return root;
+    }
+
+    public void revcse(TreeNode root){
+        if (root == null)return;
+        TreeNode temp;
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        revcse(root.left);
+        revcse(root.right);
+
+
+    }
 
 
 
-
-
-
-
-
-
-
+}
 
 
 
@@ -114,15 +171,20 @@ public class tow_tree_part {
 
 
         //中序遍历
-        new Ergodic().Middle(root);
-        List<Integer> arr =new ArrayList<>();
-        new Ergodic().Middle(root,arr);
-        System.out.println(arr);
+//        new Ergodic().Middle(root);
+//        List<Integer> arr =new ArrayList<>();
+//        new Ergodic().Middle(root,arr);
+//        System.out.println(arr);
 
         //后序遍历
 //        new Ergodic().Postorder(root);
 //        List<Integer> arr =new ArrayList<>();
 //        new Ergodic().Postorder(root,arr);
 //        System.out.println(arr);
+
+        System.out.println(new LevelOrder().levelOrder(root));
+        new InvertTree().invertTree(root);
+        System.out.println(new LevelOrder().levelOrder(root));
+
     }
 }
