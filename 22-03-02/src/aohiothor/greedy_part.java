f870e4369a6b7b72eceaa810c2e0e6c3dd5ebd90
saltfile <3205617363@qq.com>
2023-02-13 11:23:56 +0800

    02-13

--- 22-03-02/src/aohiothor/greedy_part.java
+++ 22-03-02/src/aohiothor/greedy_part.java
@@ -2,6 +2,7 @@ package aohiothor;
 
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.stream.IntStream;
 
 /**
@@ -306,9 +307,37 @@ class LemonadeChange{
     }
 }
 
+/**
+ * 406. 根据身高重建队列
+ *
+ * 假设有打乱顺序的一群人站成一个队列，数组 people 表示队列中一些人的属性（不一定按顺序）。每个 people[i] = [hi, ki] 表示第 i 个人的身高为 hi ，前面 正好 有 ki 个身高大于或等于 hi 的人。
+ *
+ * 请你重新构造并返回输入数组 people 所表示的队列。返回的队列应该格式化为数组 queue ，其中 queue[j] = [hj, kj] 是队列中第 j 个人的属性（queue[0] 是排在队列前面的人）。
+ */
+
+class ReconstructQueue{
+
+
+    public int[][] reconstructQueue(int[][] people) {
+        //先排好序
+        Arrays.sort(people, (a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            return b[0] - a[0];
+        });
+
+        LinkedList<int[]> res = new LinkedList<>();
+
+        for (int[] person : people) {
+            res.add(person[1],person);
+        }
+
+
+        return res.toArray(new int[people.length][]);
+    }
 
 
 
+}
 
 
 public class greedy_part {
