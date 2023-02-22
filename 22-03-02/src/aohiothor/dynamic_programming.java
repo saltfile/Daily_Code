b0334e74e2157edbced85aadae0d6369ae017e3e
saltfile <3205617363@qq.com>
2023-02-22 23:12:55 +0800

    02-22

--- 22-03-02/src/aohiothor/dynamic_programming.java
+++ 22-03-02/src/aohiothor/dynamic_programming.java
@@ -149,8 +149,35 @@ class RobII{
 
 }
 
+/**
+ * 121. 买卖股票的最佳时机
+ *
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ *
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ *
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ *
+ * // dp[i][0]代表第i天持有股票的最大收益
+ *         // dp[i][1]代表第i天不持有股票的最大收益
+ *
+ */
+class MaxProfit1{
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
 
 
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0],-prices[i]);
+            dp[i][1] = Math.max(dp[i-1][0]+prices[i],dp[i-1][1]);
+        }
+
+        return dp[prices.length-1][1];
+    }
+}
 
 
 
