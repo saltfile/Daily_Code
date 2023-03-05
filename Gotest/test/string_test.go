ec867e72615ca45dd1255394bab526610da2e24e
saltfile <3205617363@qq.com>
2023-03-06 01:07:03 +0800

    03-06

--- Gotest/test/string_test.go
+++ Gotest/test/string_test.go
@@ -1,6 +1,10 @@
 package test
 
-import "testing"
+import (
+	"strconv"
+	"strings"
+	"testing"
+)
 
 //测试字符串的各种差异
 
@@ -25,6 +29,36 @@ func TestStrings(t *testing.T) {
 	//fune是从字符串中自动取出utf8编码的函数
 	c := []rune(s)
 	//取出的编码20013
-	t.Log(c[0])
-	t.Log(s)
+	t.Logf("unicode %x", c[0])
+	t.Logf("utf8 %x", s)
+}
+
+// 中文字符串遍历
+func TestStingToRange(t *testing.T) {
+	s := "我是超级小淘气，面对世界很好奇"
+	for _, i2 := range s {
+		t.Logf("%[1]c %[1]x", i2)
+	}
+}
+
+//对应的字符串操作
+
+func TestStringFun(t *testing.T) {
+	s := "A,B,C D,E"
+	//切割
+	words := strings.Split(s, ",")
+	//for _, i2 := range words {
+	//	t.Log(i2)
+	//}
+	//链接
+	merg := strings.Join(words, "-")
+	t.Log(merg)
+
+	//转化整数->字符串
+	ns := strconv.Itoa(100)
+	t.Log(ns)
+
+	sn, _ := strconv.Atoi(ns)
+	t.Log(sn)
+
 }
