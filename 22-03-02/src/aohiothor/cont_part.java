df62a9db4f166b69c23bc02a08f6e0452d005833
saltfile <3205617363@qq.com>
2023-03-11 23:36:08 +0800

    03-11

--- 22-03-02/src/aohiothor/cont_part.java
+++ 22-03-02/src/aohiothor/cont_part.java
@@ -111,8 +111,78 @@ class AddDigits{
 }
 
 
+
+
+class MeiTuan{
+
+
+/**
+ * 小美有一个由数字字符组成的字符串。现在她想对这个字符串进行一些修改。
+ * 具体地，她可以将这个字符串中任意位置字符修改为任意的数字字符。
+ * 她想知道，至少进行多少次修改，可以使得修改后的字符串不包含两个连续相同的字符？
+ * 例如，对于字符串”111222333”，她可以进行3次修改将其变为”121212313”。
+ * 注意返回的是次数
+ */
+
+public int XiaoMei(String str){
+    int left = 0;
+    int res = 0;
+    char a = str.charAt(left);
+    for (int i = 1; i < str.length(); i++) {
+        if (str.charAt(i) == a) {
+            res++;
+            a = '&';
+            left = i;
+            continue;
+        }
+        left = i;
+        a = str.charAt(left);
+
+    }
+
+
+return res;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class cont_part {
+
+    private static Integer a = new Integer(4);
+
+
+
     public static void main(String[] args) {
-        System.out.println(new soult1().MaxBu("stringsabcvdb"));
+
+        MeiTuan meiTuan = new MeiTuan();
+        System.out.println(meiTuan.XiaoMei("111222333"));
+
+//        System.out.println(new soult1().MaxBu("stringsabcvdb"));
     }
 }
