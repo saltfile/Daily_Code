4b13f26640e852054f7f36c152a2dca3f7ec7c07
saltfile <3205617363@qq.com>
2023-03-18 22:32:55 +0800

    03-18

--- 22-03-02/src/aohiothor/simple_leecode.java
+++ 22-03-02/src/aohiothor/simple_leecode.java
@@ -1,6 +1,8 @@
 package aohiothor;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * 罗马数字转整数
@@ -232,7 +234,39 @@ class NumIslands{
 
 }
 
+/**
+ * 11. 盛最多水的容器
+ *
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ *
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 返回容器可以储存的最大水量。
+ *
+ * 说明：你不能倾斜容器。
+ */
 
+class MaxArea{
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+
+        int res = 0;
+
+
+        while (left < right){
+            int area = Math.min(height[left],height[right])*(right-left);
+            res = Math.max(area,res);
+
+            if (height[left] > height[right])right--;
+            else left++;
+        }
+        return res;
+
+    }
+
+}
 
 
 
@@ -243,6 +277,9 @@ class NumIslands{
 
 public class simple_leecode {
     public static void main(String[] args) {
+
+
+
 
 //        Collection<String> c = new ArrayList<String>();
 //
