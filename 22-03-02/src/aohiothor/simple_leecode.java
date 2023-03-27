63e895ccaea2aa8a117bbc5c4547b9474ed2b2ce
saltfile <3205617363@qq.com>
2023-03-27 22:26:08 +0800

    03-27

--- 22-03-02/src/aohiothor/simple_leecode.java
+++ 22-03-02/src/aohiothor/simple_leecode.java
@@ -1,5 +1,11 @@
 package aohiothor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
@@ -332,12 +338,62 @@ return true;
 }
 
 
+/**
+ * 191. 位1的个数
+ *
+ * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+ *
+ *
+ *
+ * 提示：
+ *
+ *     请注意，在某些语言（如 Java）中，没有无符号整数类型。在这种情况下，输入和输出都将被指定为有符号整数类型，并且不应影响您的实现，因为无论整数是有符号的还是无符号的，其内部的二进制表示形式都是相同的。
+ *     在 Java 中，编译器使用二进制补码记法来表示有符号整数。因此，在 示例 3 中，输入表示有符号整数 -3。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：n = 00000000000000000000000000001011
+ * 输出：3
+ * 解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
+ */
+
+class HammingWeight{
+    public int hammingWeight(int n) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                res++;
+            }
+        }
+
+
+        return res;
+    }
+}
+
+
+
+
+
 
 
 public class simple_leecode {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println(new RotateString().rotateString("abcde","cdeab"));
+//        System.out.println(new RotateString().rotateString("abcde","cdeab"));
+//        FileChannel fileChannel = new RandomAccessFile(new File("db.data"), "rw").getChannel();
+//        MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, fileChannel.size());
+//        // 写
+//        byte[] data = new byte[4];
+//        int position = 8;
+//        // 从当前 mmap 指针的位置写入 4b 的数据
+//        mappedByteBuffer.put(data);
+//        MappedByteBuffer subBuffer = (MappedByteBuffer) mappedByteBuffer.slice();
+//        subBuffer.position(position);
+//        subBuffer.put(data);
 
 
 //        Collection<String> c = new ArrayList<String>();
