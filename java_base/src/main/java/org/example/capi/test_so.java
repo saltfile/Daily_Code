6b2671c733d35fb3240f2b62b842daa6891f6e96
saltfile <3205617363@qq.com>
2023-04-06 10:58:51 +0800

    04-06

---
+++ java_base/src/main/java/org/example/capi/test_so.java
@@ -0,0 +1,15 @@
+package org.example.capi;
+
+public class test_so extends Myfunc{
+    static{
+        System.load("/opt/git_Pro/Daily_Code/java_base/src/main/java/org/example/capi/libmy_test.so");
+    }
+
+    public static void main(String[] args) {
+        test_so c = new test_so();
+        int a = c.add(4,7);
+        System.out.println(a);
+    }
+
+
+}
