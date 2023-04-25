27270443300d32d66608d8d24cf8749fe89222e9
saltfile <3205617363@qq.com>
2023-04-25 15:40:13 +0800

    04-25

--- java_base/src/main/java/org/example/testrf.java
+++ java_base/src/main/java/org/example/testrf.java
@@ -3,6 +3,7 @@ package org.example;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class testrf {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
@@ -11,6 +12,8 @@ public class testrf {
         Constructor ul = clazz.getConstructor();;
         Object obj = ul.newInstance();
         met.invoke(obj);
+
+
 
     }
 }
