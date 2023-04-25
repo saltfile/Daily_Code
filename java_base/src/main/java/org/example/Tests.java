81dbb334002ab510efa0b6a612c0cbf7873d3000
saltfile <3205617363@qq.com>
2023-04-25 14:40:00 +0800

    04-25

---
+++ java_base/src/main/java/org/example/Tests.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Tests {
+    public int xxx;
+    public void fun(){
+        System.out.println("我是Tests");
+    }
+}
---
+++ java_base/src/main/java/org/example/testrf.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class testrf {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class clazz = Class.forName("org.example.Tests");
+        Method met = clazz.getDeclaredMethod("fun");
+        Constructor ul = clazz.getConstructor();;
+        Object obj = ul.newInstance();
+        met.invoke(obj);
+
+    }
+}
+
